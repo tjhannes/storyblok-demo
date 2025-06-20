d0e4a995bced15f56737303e4acc8052462ea3ae
Johannes <johannes.thanner@produktmacher.com>
2025-06-20 22:39:42 +0200

    fix: remove server components for ssg support

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,19 +1,29 @@
-import {
-  storyblokEditable,
-  StoryblokServerComponent,
-  SbBlokData,
-} from "@storyblok/react/rsc";
+import { storyblokEditable, SbBlokData } from "@storyblok/react";
+import Feature from "./Feature";
 
 interface GridBlok extends SbBlokData {
   columns: SbBlokData[];
 }
 
+interface FeatureBlok extends SbBlokData {
+  name: string;
+}
+
 export default function Grid({ blok }: { blok: GridBlok }) {
   return (
     <div className="grid" {...storyblokEditable(blok)}>
-      {blok.columns.map((nestedBlok) => (
-        <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
-      ))}
+      {blok.columns.map((nestedBlok) => {
+        if (nestedBlok.component === "feature") {
+          return (
+            <Feature key={nestedBlok._uid} blok={nestedBlok as FeatureBlok} />
+          );
+        }
+        return (
+          <div key={nestedBlok._uid}>
+            Unknown component: {nestedBlok.component}
+          </div>
+        );
+      })}
     </div>
   );
 }
